0d74390d964c26222e22c9b97876229e3019277f
shadrajput <shadrajput6@gmail.com>
2023-07-17 17:37:56 +0530

    Responsive Hero Section // About Us

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -76,11 +76,11 @@ export function Hero() {
                   <img src={item.img} alt="fess manager logo" className='w-full h-full relative' />
                 )}
                 {index == currentIndex && (
-                  <div className='absolute top-72 left-40 w-[700px]'>
-                    <h1 className='text-[#b19777] font-bold text-[55px]'>{item.heading1}</h1>
-                    <h1 className='text-black text-[55px] font-bold'>{item.heading2}</h1>
-                    <p className='font-light pr-52 py-8 text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, odit harum, tenetur ullam est magnam fuga.</p>
-                    <button className='btn1 font-semibold text-[12px] hover:text-white z-50 group text-sm py-3 '>
+                  <div className='absolute top-32 left-3 xs:top-28 sm:top-40 sm:left-10 md:top-48 lg:top-60 lg:left-16 2xl:top-72 2xl:left-40 2xl:w-[800px]'>
+                    <h1 className='text-[#b19777] font-bold xs:text-lg sm:text-2xl md:text-3xl lg:text-4xl xl:text-[55px]'>{item.heading1}</h1>
+                    <h1 className='text-black xl:text-[55px] xs:text-lg sm:text-2xl md:text-3xl lg:text-4xl font-bold'>{item.heading2}</h1>
+                    <p className='font-light 2xl:pr-0 2xl:py-8 text-black text-xs hidden sm:block sm:py-2 sm:mr-80 md:pr-20 lg:pr-60 lg:text-base xl:pr-80 xl:py-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, odit harum, tenetur ullam est magnam fuga.</p>
+                    <button className='btn1 font-semibold text-[12px] hover:text-white border border-black z-50 group text-sm py-1 my-2 sm:py-2 xl:py-3 '>
                       Shop Now
                       <BsArrowRight className='text-black group-hover:text-white text-lg ml-3' />
                     </button>
--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -6,18 +6,18 @@ export default function Subscribe() {
         <>
             <div className='h-[400px] relative'>
                 <Image src={image} alt="fess manager logo" className='' />
-                <div className="h-[400px] w-full bg-black/70 absolute top-0 ">
+                <div className="h-[115px] sm:h-[205px] md:h-[245px] lg:h-[325px] xl:h-[400px] 2xl:h-[487px] hi w-full bg-black/70 absolute top-0 ">
                 </div>
-                <div className="flex flex-col justify-center items-center absolute top-[28%] left-[35%]">
-                    <h1 className="text-[#b19777] font-semibold text-2xl">Subscribe for Members</h1>
-                    <h1 className="text-white text-2xl font-semibold py-3">to Receive Latest News and Special Offers</h1>
-                    <form action="">
-                        <div className="flex mt-5">
+                <div className="flex flex-col justify-center items-center absolute left-6 top-3 sm:left-36 sm:top-10 lg:left-72 lg:top-20  xl:top-[30%] xl:left-[35%] ">
+                    <h1 className="text-[#b19777] font-semibold sm:text-xl md:text-2xl">Subscribe for Members</h1>
+                    <h1 className="text-white md:text-2xl sm:text-xl font-semibold xl:py-3">to Receive Latest News and Special Offers</h1>
+                    <form action="" className="">
+                        <div className="flex mt-2 md:mt-5">
                             <input type="text"
                                 placeholder="Your email..."
-                                className="px-5 w-80 focus:outline-none"
+                                className="px-5 py-[2px] sm:py-1 md:py-2 lg:py-3 md:w-80 2xl:w-96 2xl:py-4 focus:outline-none"
                             />
-                            <button className='btn3 text-sm text-white font-semibold z-50'>
+                            <button className='btn3 px-3 text-sm text-white font-semibold z-50'>
                                 Subscribe
                             </button>
                         </div>

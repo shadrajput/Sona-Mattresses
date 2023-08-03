40edb950fe06341cd2103c6799afe39e709aff5d
shadrajput <shadrajput6@gmail.com>
2023-08-03 17:51:37 +0530

    Product Details

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -8,6 +8,14 @@ import { BiRupee } from "react-icons/bi"
 import Link from 'next/link'
 
 export default function Product() {
+
+    const [custom, setcustom] = React.useState(false)
+
+    const handleChange = () => {
+        setcustom(true)
+    };
+
+
     return (
         <>
             <Header />
@@ -17,36 +25,115 @@ export default function Product() {
                         <Image src={mattrest} alt="fess manager logo" className='rounded-lg ' />
                     </div>
                     <div className='lg:pr-20'>
-                        <h1 className='text-[#b19777] font-semibold text-4xl'>Sona Mattrest</h1>
-                        <p className='font-light py-4'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                        <div className='flex items-center text-3xl font-semibold'>
-                            <BiRupee />
-
-                            <h1>
-                                549.00
-                            </h1>
-                        </div>
-                        <div className='flex items-center text-xl font-semibold space-x-3 py-5'>
-                            <span className='text-2xl text-[#b19777]'>Size  : </span>
-                            <span className='border border-[#b19777] px-3 shadow-sm rounded-lg'>4</span>
-                            <span>&</span>
-                            <span className='border border-[#b19777] px-3 shadow-sm rounded-lg'>5</span>
-                        </div>
-                        <div className='flex items-center text-xl font-semibold space-x-3 py-2'>
-                            <span className='text-2xl text-[#b19777]'>Materials  : </span>
-                            <span >Spring</span>
+                        <h1 className='text-[#b19777] font-semibold text-4xl'>Pocket Spring Mattress</h1>
+                        <p className='font-light py-4 text-sm'>
+                            Pocket spring mattresses offer numerous benefits, making them a popular choice for many individuals. These mattresses are designed to provide exceptional support and comfort by using individual pocketed springs that respond independently to the body's weight and shape, promoting proper spinal alignment and reducing pressure points.</p>
+                        <div>
+                            <h1 className='text-[#b19777] text-lg'>Features</h1>
+                            <ul className='text-[14px] font-light text-slate-600 space-y-1 '>
+                                <li className=''>Knitted jacquared layers</li>
+                                <li className=''>High Density foam</li>
+                                <li className=''>7 Years Warranty</li>
+                            </ul>
                         </div>
                         <div>
-                            <Link href="/ContactUs">
-                                <button className='btn3 font-semibold text-[12px] text-white z-50 group '>
-                                    Shop Now
-                                </button>
-                            </Link>
+                            <div className='flex items-center space-x-14 py-4'>
+                                <div className='flex flex-col w-full'>
+                                    <label htmlFor="" className='text-[14px] text-[#b19777] font-light'>Size</label>
+                                    <select name="" id="" className='py-[2px] text-sm w-full mt-2 border-slate-400 rounded-sm text-slate-500'>
+                                        <option value="Regular">Regular</option>
+                                        <option value="Single">Single</option>
+                                        <option value="Double">Double</option>
+                                        <option value="King">King</option>
+                                        <option value="Queen">Queen</option>
+                                    </select>
+                                </div>
+                                <div className='flex flex-col w-full'>
+                                    <label htmlFor="" className='text-[14px] text-[#b19777] font-light'>Dimension</label>
+                                    <select name="" id="" className='py-[2px] text-sm w-full mt-2 border-slate-400 rounded-sm text-slate-500'>
+                                        <option value="Regular">Regular</option>
+                                        <option value="Single">Single</option>
+                                        <option value="Double">Double</option>
+                                        <option value="King">King</option>
+                                        <option value="Queen">Queen</option>
+                                    </select>
+                                </div>
+                                <div className='flex-col flex w-full'>
+                                    <label htmlFor="" className='text-[14px] text-[#b19777] font-light'>Height</label>
+                                    <select name="" id="" className='py-[2px] text-sm w-full mt-2 border-slate-400 rounded-sm text-slate-500'>
+                                        <option value="Regular">Regular</option>
+                                        <option value="Single">Single</option>
+                                        <option value="Double">Double</option>
+                                        <option value="King">King</option>
+                                        <option value="Queen">Queen</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className='flex items-center py-3 space-x-2 '>
+                                <div className=''>
+                                    <input type="checkbox"
+                                        value="checked"
+                                        className='cursor-pointer rounded-sm text-xs h-[13px] focus:outline-none w-[13px]'
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div>
+                                    <h1 className='font-semibold text-sm uppercase text-[#b19777]'>Custom size</h1>
+                                </div>
+                            </div>
+                            <div className={` ${custom == false ? "hidden" : "flex"} items-center space-x-14 py-4`}>
+                                <div className='flex flex-col w-full'>
+                                    <label htmlFor="" className='text-[14px] text-[#b19777] font-light'>Width</label>
+                                    <select name="" id="" className='py-[2px] text-sm w-full mt-2 border-slate-400 rounded-sm text-slate-500'>
+                                        <option value="Regular">Regular</option>
+                                        <option value="Single">Single</option>
+                                        <option value="Double">Double</option>
+                                        <option value="King">King</option>
+                                        <option value="Queen">Queen</option>
+                                    </select>
+                                </div>
+                                <div className='flex flex-col w-full'>
+                                    <label htmlFor="" className='text-[14px] text-[#b19777] font-light'>Length</label>
+                                    <select name="" id="" className='py-[2px] text-sm w-full mt-2 border-slate-400 rounded-sm text-slate-500'>
+                                        <option value="Regular">Regular</option>
+                                        <option value="Single">Single</option>
+                                        <option value="Double">Double</option>
+                                        <option value="King">King</option>
+                                        <option value="Queen">Queen</option>
+                                    </select>
+                                </div>
+                                <div className='flex-col flex w-full'>
+                                    <label htmlFor="" className='text-[14px] text-[#b19777] font-light'>Height</label>
+                                    <select name="" id="" className='py-[2px] text-sm w-full mt-2 border-slate-400 rounded-sm text-slate-500'>
+                                        <option value="Regular">Regular</option>
+                                        <option value="Single">Single</option>
+                                        <option value="Double">Double</option>
+                                        <option value="King">King</option>
+                                        <option value="Queen">Queen</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='flex justify-center items-center space-x-5'>
+                            <div className='w-full'>
+                                <Link href="/ContactUs">
+                                    <button className='btn3 font-semibold text-[12px] text-white z-50 group w-full py-3 mt-5 '>
+                                        Connect with Us
+                                    </button>
+                                </Link>
+                            </div>
+                            <div className='w-full'>
+                                <Link href="/ContactUs">
+                                    <button className='btn3 font-semibold text-[12px] text-white z-50 group w-full py-3 mt-5 '>
+                                        Shop Now
+                                    </button>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className='lg:px-40 lg:pt-10 flex flex-col justify-center w-full items-center px-10 '>
+            <div className='lg:px-40 lg:pt-20 flex flex-col justify-center w-full items-center px-10 '>
                 <div>
                     <h1 className='text-center font-semibold'>Description </h1>
                     <div className=' flex justify-center items-center py-2'>

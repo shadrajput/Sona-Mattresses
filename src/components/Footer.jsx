d93aa82c491e3e516b5d87176d16b42d3257b012
Deepak Prajapati <prajapatideepak073@gmail.com>
2023-08-21 20:27:28 +0530

    footer nav

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -38,23 +38,29 @@ export function Footer() {
                 Footer Menu
               </h1>
               <nav className="space-y-3 ">
-                <li className="flex cursor-pointer items-center space-x-3 text-[13px] text-white duration-300 hover:text-[#ae9775] xs:text-gray-300 md:text-sm">
+                <li className="flex cursor-pointer items-center space-x-3 font-semibold text-white duration-300 hover:text-[#ae9775]">
                   <BiChevronRight className="font-bold text-[#b19777]" />
-                  <h1>AboutUs</h1>
+                  <Link href={`/AboutUs`}>
+                    <h1>About Us</h1>
+                  </Link>
                 </li>
-                <li className="flex cursor-pointer items-center space-x-3 text-[13px] text-white duration-300  hover:text-[#ae9775] xs:text-gray-300 md:text-sm">
+                <li className="flex cursor-pointer items-center space-x-3 font-semibold  text-white duration-300 hover:text-[#ae9775]">
                   <BiChevronRight className="font-bold text-[#b19777]" />
-                  <h1>ContactUs</h1>
+                  <Link href={`/ContactUsv`}>
+                    <h1>Contact Us</h1>
+                  </Link>
                 </li>
-                <li className="flex cursor-pointer items-center space-x-3 text-[13px] text-white duration-300 hover:text-[#ae9775] xs:text-gray-300 md:text-sm">
+                <li className="flex cursor-pointer items-center space-x-3 font-semibold text-white duration-300 hover:text-[#ae9775]">
                   <BiChevronRight className="font-bold text-[#b19777]" />
-                  <h1>Product</h1>
+                  <Link href={`/Product`}>
+                    <h1>Product</h1>
+                  </Link>
                 </li>
-                <li className="flex cursor-pointer items-center space-x-3 text-[13px] text-white duration-300 hover:text-[#ae9775] xs:text-gray-300 md:text-sm">
+                <li className="flex cursor-pointer items-center space-x-3 font-semibold text-white duration-300 hover:text-[#ae9775]">
                   <BiChevronRight className="font-bold text-[#b19777]" />
                   <h1>Shipping Policy</h1>
                 </li>
-                <li className="flex cursor-pointer items-center space-x-3 text-[13px] text-white duration-300 hover:text-[#ae9775] xs:text-gray-300 md:text-sm">
+                <li className="flex cursor-pointer items-center space-x-3 font-semibold text-white duration-300 hover:text-[#ae9775]">
                   <BiChevronRight className="font-bold text-[#b19777]" />
                   <h1>Refund Policy</h1>
                 </li>
